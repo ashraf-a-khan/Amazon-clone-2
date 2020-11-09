33ffe6487a7436802f0dfe9f7cccf0b747631702
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-11-09 15:17:35 -0500

    Adds proceed to checkout option

--- Amazon-clone-main/src/App.js
+++ Amazon-clone-main/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Payment from "./Payment";
 import Login from "./Login";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
@@ -43,6 +44,10 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
           </Route>
           <Route path="/">
             <Header />
